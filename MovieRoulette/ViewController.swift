422842cea4b48a02397525dbe9627fbdc36ee4db
Michael Maryanoff <mmaryanoff@gmail.com>
2019-08-20 12:57:55 -0500

    Initial Commit

---
+++ MovieRoulette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieRoulette
+//
+//  Created by Michael Maryanoff on 8/20/19.
+//  Copyright © 2019 Michael Maryanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
