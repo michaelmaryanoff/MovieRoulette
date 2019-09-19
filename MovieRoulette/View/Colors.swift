6f045d9888752a0d7afada970f3e54f7cc28c363
MichaelMaryanoff <mmaryanoff@gmail.com>
2019-09-19 13:17:07 -0500

    Add comments to Colors struct

--- MovieRoulette/View/Colors.swift
+++ MovieRoulette/View/Colors.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 
+// Creates colors to be accessed globally in order to maintain a consistent color theme
 struct Colors {
     static let richBlue = UIColor(red: 0.0/255.0, green: 2.0/255.0, blue: 114.0/255.0, alpha: 1.0)
     static let darkPurple = UIColor(red: 52.0/255.0, green: 22.0/255.0, blue: 119.0/255.0, alpha: 1.0)
