3bdd172d33bfced1c582219d175b5899afbabd72
MichaelMaryanoff <mmaryanoff@gmail.com>
2019-09-19 13:18:02 -0500

    Add comments to CustomActorCell

--- MovieRoulette/View/CustomActorCell.swift
+++ MovieRoulette/View/CustomActorCell.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 
+// Custom cell for the ActorSearchViewController
 class CustomActorCell: UITableViewCell {
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
