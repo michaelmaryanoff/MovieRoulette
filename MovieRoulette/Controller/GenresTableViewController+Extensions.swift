98a05ed296d0260d0580f5c1ecac6db02656f574
MichaelMaryanoff <mmaryanoff@gmail.com>
2019-09-19 18:39:55 -0500

    Move tableview functions to separate extension class

---
+++ MovieRoulette/Controller/GenresTableViewController+Extensions.swift
@@ -0,0 +1,72 @@
+//
+//  GenresTableViewController+Extensions.swift
+//  MovieRoulette
+//
+//  Created by Michael Maryanoff on 9/19/19.
+//  Copyright © 2019 Michael Maryanoff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension GenresTableViewController {
+    
+    // MARK: - Table view data source methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return GenreConstants.genresArray.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "genreCell", for: indexPath)
+        
+        setupTableViewCell(cell: cell)
+        
+        cell.textLabel?.text = GenreConstants.genresArray[indexPath.row]
+        
+        for item in GenresTableViewController.managedGenreArray {
+            
+            if let itemString = item.genreName {
+                if cell.textLabel?.text == itemString {
+                    cell.accessoryType = .checkmark
+                }
+                
+            }
+            
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard let currentCell = tableView.cellForRow(at: indexPath) else {
+            print("cannot with current cell")
+            return
+        }
+        
+        if currentCell.accessoryType == .checkmark {
+            currentCell.accessoryType = .none
+            changeManagedGenreSet(forCell: currentCell, add: false, indexPath: indexPath)
+        } else {
+            currentCell.accessoryType = .checkmark
+            changeManagedGenreSet(forCell: currentCell, add: true, indexPath: indexPath)
+        }
+        
+    }
+    
+    func setupTableViewCell(cell: UITableViewCell) {
+        let fontDescriptor = UIFontDescriptor(fontAttributes: [.family: "Arial Rounded MT Bold"])
+        
+        cell.textLabel?.font = UIFont(descriptor: fontDescriptor, size: 16)
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = Colors.pinkOrange
+        cell.textLabel?.shadowColor = .black
+        cell.textLabel?.shadowOffset = CGSize(width: 0.9, height: 0.9)
+    }
+    
+}
--- MovieRoulette/Controller/GenresTableViewController.swift
+++ MovieRoulette/Controller/GenresTableViewController.swift
@@ -47,59 +47,7 @@ class GenresTableViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
 
-    // MARK: - Table view data source methods
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return GenreConstants.genresArray.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell = tableView.dequeueReusableCell(withIdentifier: "genreCell", for: indexPath)
-        
-        let fontDescriptor = UIFontDescriptor(fontAttributes: [.family: "Arial Rounded MT Bold"])
-        
-        cell.textLabel?.font = UIFont(descriptor: fontDescriptor, size: 16)
-        cell.textLabel?.textColor = .white
-        cell.backgroundColor = Colors.pinkOrange
-        cell.textLabel?.shadowColor = .black
-        cell.textLabel?.shadowOffset = CGSize(width: 0.9, height: 0.9)
-        
-        
-        cell.textLabel?.text = GenreConstants.genresArray[indexPath.row]
-        
-        for item in GenresTableViewController.managedGenreArray {
-            
-            if let itemString = item.genreName {
-                if cell.textLabel?.text == itemString {
-                    cell.accessoryType = .checkmark
-                }
-
-            }
-
-        }
-        return cell
-    }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-        guard let currentCell = tableView.cellForRow(at: indexPath) else {
-            print("cannot with current cell")
-            return
-        }
-        
-        if currentCell.accessoryType == .checkmark {
-            currentCell.accessoryType = .none
-            changeManagedGenreSet(forCell: currentCell, add: false, indexPath: indexPath)
-        } else {
-            currentCell.accessoryType = .checkmark
-            changeManagedGenreSet(forCell: currentCell, add: true, indexPath: indexPath)
-        }
-
-    }
     
     // Reusable function that changes the context
     func changeManagedGenreSet(forCell cell: UITableViewCell, add: Bool, indexPath: IndexPath) {
