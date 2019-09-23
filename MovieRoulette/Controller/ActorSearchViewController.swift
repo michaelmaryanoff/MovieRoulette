57c83cfe6903313b0f5e6677394519c9b9d690bb
MichaelMaryanoff <mmaryanoff@gmail.com>
2019-09-23 12:32:52 -0500

    Move search bar functions to extension file

--- MovieRoulette/Controller/ActorSearchViewController.swift
+++ MovieRoulette/Controller/ActorSearchViewController.swift
@@ -61,64 +61,11 @@ class ActorSearchViewController: UIViewController  {
             
         }
     }
-}
-
-
-//MARK: - Search bar deleage functions
-extension ActorSearchViewController: UISearchBarDelegate {
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if CheckConnectivity.isConnectedToInternet == false {
-            DispatchQueue.main.async {
-                self.presentAlertControllerDismiss(title: "There is no internet connection!", message: "Please check your connection and try again.")
-                self.changeActivityIndicatorState(isAnimating: false)
-            }
-            
-        }
-
-        self.changeActivityIndicatorState(isAnimating: true)
-
-        TMDBClient.searchForActorID(query: searchText) { (success, actorStringArray, idIntArray, error) in
-        
-            
-            if !actorStringArray.isEmpty {
-                self.changeActivityIndicatorState(isAnimating: false)
-            }
-            
-            if CheckConnectivity.isConnectedToInternet == false {
-                DispatchQueue.main.async {
-                    self.presentAlertControllerDismiss(title: "There is no internet connection!", message: "Please check your connection and try again.")
-                    self.changeActivityIndicatorState(isAnimating: false)
-                }
-                
-            }
-            
-            if actorStringArray.isEmpty && !searchText.isEmpty {
-                DispatchQueue.main.async {
-                    self.presentAlertControllerDismiss(title: "Could not find any actors.", message: "Please try again.")
-                }
-                
-                
-            }
-        
-            self.actors = actorStringArray
-            self.actorsIdArray = idIntArray
-            
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-                
-            }
-
-            
-            
-        }
-        
-    }
-    
-    
     public func presentAlertControllerDismiss(title: String, message: String) -> Void {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
         self.present(alertController, animated: true)
     }
 }
+
+
