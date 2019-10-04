23d4eb101cc55dbc0866e1453c742bbe05959b58
MichaelMaryanoff <mmaryanoff@gmail.com>
2019-10-04 17:46:01 -0500

    Seperate API Key

---
+++ MovieRoulette/Model/APIKey.swift
@@ -0,0 +1,14 @@
+//
+//  APIKey.swift
+//  MovieRoulette
+//
+//  Created by Michael Maryanoff on 10/4/19.
+//  Copyright © 2019 Michael Maryanoff. All rights reserved.
+//
+
+import Foundation
+
+struct APIKey {
+    
+    static let apiKey = "YOUR_API_KEY"
+}
